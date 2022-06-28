43dd02bb973f32c2eba5598a5f5901f7adaa82a0
{buhuykiuyn} <uyenbhku@gmail.com>
2022-06-28 18:22:09 +0700

    Create Header.cpp

---
+++ Sources/Sorting/Header.cpp
@@ -0,0 +1,18 @@
+#include "../../Header/Header.hpp"
+
+template<typename T>
+void readFile(T *&a, T &n, int index){
+    // open input file 
+    ifstream isFile("../../Input/inputF"+to_string(index)+".txt");
+    if (isFile.fail()) {
+        cout << "\nCannot open input file";
+        exit(1);
+    }
+
+    // get input data 
+    isFile >> n; // read size
+    a = new int [n];
+    int i = 0;
+    while (isFile >> a[i]) i++; // read numbers
+    isFile.close();
+}
