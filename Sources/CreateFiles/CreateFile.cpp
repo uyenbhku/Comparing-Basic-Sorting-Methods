3103c3036a2b1ab0c232d36d6fb62049f513afa3
{buhuykiuyn} <uyenbhku@gmail.com>
2022-06-28 16:23:44 +0700

    Create CreateFile.cpp
    CreateFile will generate 9 files containing:
    - The first line is a positive integer standing for the number of random positive integers in the array later
    - Other lines are integers stored in the array

---
+++ Sources/CreateFiles/CreateFile.cpp
@@ -0,0 +1,35 @@
+#include "../Header/Header.hpp"
+
+#define RAND_RANGE 2031312329
+
+int getRandom()
+{ 
+    return rand() % RAND_RANGE;
+}
+
+void createAFile(const int& size, const int& index){
+    ofstream osFile("inputF"+to_string(index)+".txt");  // open/create an output file
+    if (!osFile.fail()){
+        osFile << size << endl;
+        // create a file with random numbers
+        for (int i=0; i<size; i++){ 
+            osFile << getRandom() << endl;
+        }
+    }
+    else cout << "\nCannot open file";
+    osFile.close();
+}
+
+int main()
+{
+    srand(time(NULL));
+    // int size; cout<<"\nSize of file: "; cin >> size;    // input size of input file
+    int size = 100;
+    for (int i = 1; i <= 10; i++)
+    {
+        createAFile(size, i);
+        size *= 10;
+    }
+
+    return 0;
+}
